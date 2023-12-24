9d9ad235a16dded773bb65a4148c35ae52b73cf8
SeaOfTorment <projectzero.g@gmail.com>
2023-12-24 02:03:28 -0500

    optimized

--- levels/scenes/factory.gd
+++ levels/scenes/factory.gd
@@ -1,18 +1,21 @@
 extends Node3D
 
 var red_gift = preload("res://components/prefabs/red_pres_rig.tscn")
-var green_gift = preload("res://components/prefabs/red_pres_rig.tscn")
+var green_gift = preload("res://components/prefabs/green_pres_rig.tscn")
+var snowman = preload("res://components/prefabs/snowman_rig.tscn")
+var banana = preload("res://components/prefabs/banana_inh.tscn")
+var bear = preload("res://components/prefabs/bear_inh.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var arr = [red_gift, green_gift]
+	var arr = [red_gift, green_gift, snowman, banana, bear]
 	var markers = $boxes_pawn_points.get_children()
 	while(true):
 		var rng = RandomNumberGenerator.new()
-		var gift_inst = arr[rng.randf_range(0, 2)].instantiate()
+		var gift_inst = arr[rng.randi_range(0, arr.size()-1)].instantiate()
 		$gift_boxes.add_child(gift_inst)
 		rng = RandomNumberGenerator.new()
-		gift_inst.position = markers[rng.randf_range(0, 6)].position
+		gift_inst.position = markers[rng.randi_range(0, 5)].position
 		await get_tree().create_timer(0.35).timeout
 
 
